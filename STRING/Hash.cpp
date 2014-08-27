845da364c766d343923590895a3d85a638861f96
码代码的猿猿 <qhn999@2008.sina.com>
2014-08-28 01:40:08 +0800

    Create Hash.cpp

---
+++ STRING/Hash.cpp
@@ -0,0 +1,24 @@
+
+/*简单的字符串has*/
+
+ull xp[maxn],hash[maxn];
+
+void init()
+{
+	xp[0]=1;
+	for(int i=1;i<maxn;i++)
+		xp[i]=xp[i-1]*175;
+}
+
+ull get_hash(int i,int L)
+{
+	return hash[i]-hash[i+L]*xp[L];
+}
+
+scanf("%s",str);
+int n=strlen(str);
+hash[n]=0;
+for(int i=n-1;i>=0;i--)
+{
+	hash[i]=hash[i+1]*175+(str[i]-'a'+1);
+}
