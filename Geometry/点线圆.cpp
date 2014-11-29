95497ff20af4e6b227432bd07500d1eb733a4c2e
码代码的猿猿 <qhn999@2008.sina.com>
2014-11-29 22:32:05 +0800

    Update 点线圆.cpp

--- Geometry/点线圆.cpp
+++ Geometry/点线圆.cpp
@@ -299,6 +299,23 @@ Circle InscribedCircle(Point p1,Point p2,Point p3)
 
 double RtoDegree(double x) {return x/pi*180.;}
 
+
+/// 两圆的面积交
+double Circle_Circle_Area_of_overlap(Circle c1, Circle c2)
+{
+    double d=Length(c1.c-c2.c);
+    if(dcmp(c1.r+c2.r-d)<=0) return 0.;
+    if(dcmp(fabs(c1.r-c2.r)-d)>=0)
+    {
+        double minr = min(c1.r,c2.r);
+        return pi*minr*minr;
+    }
+    double x = (d*d + c1.r*c1.r - c2.r*c2.r)/(2*d);
+    double t1 = acos(x/c1.r);
+    double t2 = acos((d-x)/c2.r);
+    return c1.r*c1.r*t1 + c2.r*c2.r*t2 - d*c1.r*sin(t1);
+}
+
 char op[200];
 double a[10];
 Point v[10];
