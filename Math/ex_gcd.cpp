e085514164ca1262567bd49554613c64bef3efef
码代码的猿猿 <qhn999@2008.sina.com>
2015-03-05 12:03:07 +0800

    Update ex_gcd.cpp

--- Math/ex_gcd.cpp
+++ Math/ex_gcd.cpp
@@ -26,3 +26,30 @@ LL inv(int a,int n)
   else return (x%mod+mod)%mod;
 }
 
+/* 预处逆元 及 组合数的模 */
+
+LL inv[maxn];  
+LL jc[maxn],jcv[maxn];  // 阶乘 阶乘逆
+  
+void init()  
+{  
+    /// inv jc
+    inv[1]=1; jc[0]=1; jcv[0]=1;   
+    jc[1]=1; jcv[1]=1;
+    for(int i=2;i<maxn;i++)  
+    {  
+        inv[i]=inv[mod%i]*(mod-mod/i)%mod;  
+        jc[i]=(jc[i-1]*i)%mod;  
+        jcv[i]=(jcv[i-1]*inv[i])%mod;  
+    }  
+}  
+  
+LL COMB(LL n,LL m)  
+{  
+    if(m<0||m>n) return 0LL;  
+    if(m==0||m==n) return 1LL;  
+    /// n!/((n-m)!*m!)  
+    LL ret=((jc[n]*jcv[n-m])%mod*jcv[m])%mod;  
+    return ret;  
+}  
+
