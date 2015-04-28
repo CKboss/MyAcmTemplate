77f6b7d8ad359adb434bc0ab889e0f036293c9a8
码代码的猿猿 <qhn999@2008.sina.com>
2015-04-28 17:26:44 +0800

    Create 生成树计数.cpp

---
+++ Graph/生成树计数.cpp
@@ -0,0 +1,55 @@
+/*
+Matrix-Tree 定理(Kirchhoff 矩阵-树定理)
+1、G 的度数矩阵 D[G]是一个 n*n 的矩阵,并且满足:当 i≠j 时,dij=0;当 i=j 时,dij 等于 vi 的度数。
+2、G 的邻接矩阵 A[G]也是一个 n*n 的矩阵, 并且满足:如果 vi、vj 之间有边直接相连,则 aij=1,否则
+为 0。
+我们定义 G 的 Kirchhoff 矩阵(也称为拉普拉斯算子)C[G]为 C[G]=D[G]-A[G],则 Matrix-Tree 定理可以
+描述为: G 的所有不同的生成树的个数等于其 Kirchhoff 矩阵 C[G]任何一个 n-1 阶主子式的行列式的绝对
+值。所谓 n-1 阶主子式,就是对于 r(1≤r≤n),将 C[G]的第 r 行、第 r 列同时去掉后得到的新矩阵,用 Cr[G]
+表示。
+*/
+
+
+typedef long double LD;  
+  
+const LD eps=1e-10;  
+  
+const int maxn=60;  
+  
+int sgn(LD x)  
+{  
+    if(fabs(x)<eps) return 0; if(x<0) return -1; return 1;  
+}  
+  
+LD b[maxn][maxn];  
+  
+LD det(LD a[][maxn],int n)  
+{  
+    int sign=0;  
+    int i,j,k;  
+    LD ret=1;  
+    for(i=0;i<n;i++)  
+        for(j=0;j<n;j++)  
+            b[i][j]=a[i][j];  
+    for(i=0;i<n;i++)  
+    {  
+        if(sgn(b[i][i])==0)  
+        {  
+            for(j=i+1;j<n;j++)  
+                if(sgn(b[j][i])!=0)  
+                    break;  
+            if(j==n) return 0;  
+            for(k=i;k<n;k++)  
+                swap(b[i][k],b[j][k]);  
+            sign++;  
+        }  
+        ret*=b[i][i];  
+        for(k=i+1;k<n;k++)  
+            b[i][k]/=b[i][i];  
+        for(j=i+1;j<n;j++)  
+            for(k=i+1;k<n;k++)  
+                b[j][k]-=b[j][i]*b[i][k];  
+    }  
+    if(sign&1) ret=-ret;  
+    return ret;  
+}  
