fc614dd7fa2873ecf5a07907d1ed15f1d2a84fa0
码代码的猿猿 <qhn999@2008.sina.com>
2015-04-29 09:20:55 +0800

    Update 生成树计数.cpp

--- Graph/生成树计数.cpp
+++ Graph/生成树计数.cpp
@@ -57,42 +57,43 @@ LD det(LD a[][maxn],int n)
 
 /// 取mod版
 
-LL inv(LL a,LL m)
-{
-	if(a==1) return 1;
-	return inv(m%a,m)*(m-m/a)%m;
-}
-
-LL det(LL a[][maxn],int n)
-{
-	for(int i=0;i<n;i++)
-		for(int j=0;j<n;j++)
-			b[i][j]=(a[i][j]+MOD)%MOD;
-	int res=1;
-	for(int i=0;i<n;i++)
-	{
-		for(int j=i;j<n;j++)
-		{
-			if(b[j][i]!=0)
-			{
-				for(int k=i;k<n;k++)
-					swap(b[i][k],b[j][k]);
-				if(i!=j)
-					res=(MOD-res)%MOD;
-				break;
-			}
-		}
-		if(b[i][i]==0)
-		{
-			res=-1; break;
-		}
-		for(int j=i+1;j<n;j++)
-		{
-			LL mut=(b[j][i]*inv(b[i][i],MOD))%MOD;
-			for(int k=i;k<n;k++)
-				b[j][k]=(b[j][k]-(b[i][k]*mut)%MOD+MOD)%MOD;
-		}
-		res=(res*b[i][i])%MOD;
-	}
-	return res;
-}
+LL inv(LL a,LL m)  
+{  
+    if(a==1) return 1;  
+    return inv(m%a,m)*(m-m/a)%m;  
+}  
+  
+LL det(LL a[][maxn],int n)  
+{  
+    for(int i=0;i<n;i++)  
+        for(int j=0;j<n;j++)  
+            b[i][j]=(a[i][j]+MOD)%MOD;  
+    int res=1;  
+    for(int i=0;i<n;i++)  
+    {  
+        for(int j=i;j<n;j++)  
+        {  
+            if(b[j][i]!=0)  
+            {  
+                for(int k=i;k<n;k++)  
+                    swap(b[i][k],b[j][k]);  
+                if(i!=j)  
+                    res=(MOD-res)%MOD;  
+                break;  
+            }  
+        }  
+        if(b[i][i]==0)  
+        {  
+            res=-1; break;  
+        }  
+        for(int j=i+1;j<n;j++)  
+        {  
+            LL mut=(b[j][i]*inv(b[i][i],MOD))%MOD;  
+            for(int k=i;k<n;k++)  
+                b[j][k]=(b[j][k]-(b[i][k]*mut)%MOD+MOD)%MOD;  
+        }  
+        res=(res*b[i][i])%MOD;  
+    }  
+    return res;  
+}  
+  
