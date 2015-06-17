f0859017dd89f3632ddd2077af76f1eca4d75403
码代码的猿猿 <qhn999@2008.sina.com>
2015-06-17 23:43:46 +0800

    Update SAM.cpp

--- STRING/SAM.cpp
+++ STRING/SAM.cpp
@@ -1,5 +1,16 @@
 /*Suffix Automaton*/
 
+/** 一点性质
+ * 1.代码中 p->len 变量，它表示该状态能够接受的最长的字符串长度。
+ *     该状态能够接受的最短的字符串长度。实际上等于该状态的 fa 指针指向的结点的 len + 1
+ *      (p->len)-(p->fa->len)：表示该状态能够接受的不同的字符串数,不同的字符串之间是连续的,
+ *      既:p 和 p->fa 之间 有最长的公共后缀长度 p->fa->len
+ * 2. num 表示这个状态在字符串中出现了多少次，该状态能够表示的所有字符串均出现过 num 次
+ * 3. 序列中第i个状态的子结点必定在它之后，父结点必定在它之前。
+ *     既然p出现过，那么p->fa肯定出现过。因此对一个点+1就代表对整条fa链+1.
+ * 4. 从root到每一个接收态表示一个后缀,到每一个普通节点表示一个子串
+*/
+
 const int CHAR=26,maxn=251000;
 
 struct SAM_Node
@@ -71,39 +82,26 @@ void SAM_build(char *s)
 }
 
 
-/*拓扑排序和Left,Right,num*/
-
-
- ///get tupo sort
-    for(int i=0;i<SAM_size;i++)
-        c[SAM_node[i].len]++;
-    for(int i=1;i<=len;i++)
-        c[i]+=c[i-1];
-    for(int i=0;i<SAM_size;i++)
-        top[--c[SAM_node[i].len]]=&SAM_node[i];
-
-    ///get L,R,num
-    SAM_Node *p=SAM_root;
-    for(;p->len!=len;p=p->next[str[p->len]-'a'])
-    {
-        num[p->id]=1;
-        L[p->id]=R[p->id]=p->len;
-    }
-    for(int i=SAM_size-1;i>=0;i--)
-    {
-        p=top[i];
-        if(L[p->id]==0&&R[p->id]==0)
-        {
-            L[p->id]=R[p->id]=p->pos;
-        }
-        if(p->fa)
-        {
-            SAM_Node *q=p->fa;
-            num[q->id]+=num[p->id];
-            if(L[q->id]==0||L[q->id]>L[p->id])
-                L[q->id]=L[p->id];
-            if(R[q->id]==0||R[q->id]<R[p->id])
-                R[q->id]=R[p->id];
-        }
-    }
-
+/// !!!!!!!!!!!!! 统计每个节点出现的次数  
+  
+int c[maxn],num[maxn];  
+SAM_Node* top[maxn];  
+  
+void Count(char str[],int len)  
+{  
+    for(int i=0;i<SAM_size;i++) c[SAM_node[i].len]++;  
+    for(int i=1;i<=len;i++) c[i]+=c[i-1];  
+    for(int i=0;i<SAM_size;i++) top[--c[SAM_node[i].len]]=&SAM_node[i];  
+  
+    SAM_Node *p=SAM_root;  
+    for(;p->len!=len;p=p->next[str[p->len]-'a']) num[p->id]=1; num[p->id]=1;  
+  
+    for(int i=SAM_size-1;i>=0;i--)  
+    {  
+        p=top[i];  
+        if(p->fa)  
+        {  
+            SAM_Node *q=p->fa; num[q->id]+=num[p->id];  
+        }  
+    }  
+}  
