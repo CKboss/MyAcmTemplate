490d64546b2c307580b85453ad733df3fec1e9f4
码代码的猿猿 <qhn999@2008.sina.com>
2015-07-07 20:09:27 +0800

    Update 最小树形图.cpp

--- Graph/最小树形图.cpp
+++ Graph/最小树形图.cpp
@@ -1,3 +1,12 @@
+/*
+朱刘算法 最小树形图:
+
+最小树形图无根树:
+建立一个虚拟的根节点,向所有节点连边,权值为其他所有边的权值和+1
+在求最小树形图的时候,记录和虚拟的根相连的是哪个节点,模版中边优化就不能用了
+*/
+
+
 const int INF=0x3f3f3f3f;  
 const int maxn=110;  
   
@@ -13,7 +22,7 @@ int pre[maxn],id[maxn],vis[maxn],in[maxn];
   
 int zhuliu(int root,int n,int m,Edge edge[])  
 {  
-    int res=0,u,v;  
+    int res=0;  
     while(true)  
     {  
         for(int i=0;i<n;i++) in[i]=INF;  
@@ -51,13 +60,13 @@ int zhuliu(int root,int n,int m,Edge edge[])
             if(id[i]==-1) id[i]=tn++;  
         for(int i=0;i<m;)  
         {  
-            v=edge[i].v;  
+            int v=edge[i].v;  
             edge[i].u=id[edge[i].u];  
             edge[i].v=id[edge[i].v];  
             if(edge[i].u!=edge[i].v)  
                 edge[i++].cost-=in[v];  
             else  
-                swap(edge[i],edge[--m]);  
+                swap(edge[i],edge[--m]);  /// 输出根节点的时候删掉这句
         }  
         n=tn;  
         root=id[root];  
